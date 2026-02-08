cbf18dcbde94f3ba7e817f8ca406b279f4a1f075
Devapriya C C <devapriyacc@users.noreply.huggingface.co>
2026-02-08 17:12:49 +0000

    Update src/visualization/visualizer.py

--- src/visualization/visualizer.py
+++ src/visualization/visualizer.py
@@ -6,7 +6,13 @@ import numpy as np
 def plot_chl_bloom(ds, bloom_mask):
     fig = plt.figure(figsize=(6,6))
     ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.pcolormesh(ds.longitude, ds.latitude, np.log1p(ds.chl), cmap="viridis")
+    ax.pcolormesh(
+        ds.longitude,
+        ds.latitude,
+        np.log1p(ds.chl),
+        cmap="viridis",
+        transform=ccrs.PlateCarree()
+    )
     ax.contour(ds.longitude, ds.latitude, bloom_mask, colors="red", linewidths=0.5)
     ax.coastlines()
     ax.add_feature(cfeature.LAND, facecolor="lightgray")
