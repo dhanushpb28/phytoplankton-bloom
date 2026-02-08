7c19f13ebfd49e5770845c5941e3d0ed844d4faf
Devapriya C C <devapriyacc@users.noreply.huggingface.co>
2026-02-08 17:05:34 +0000

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 import os
 import copernicusmarine
 
-def login():
+def copernicus_login():
     copernicusmarine.login(
         username=os.getenv("COPERNICUS_USERNAME"),
         password=os.getenv("COPERNICUS_PASSWORD")
